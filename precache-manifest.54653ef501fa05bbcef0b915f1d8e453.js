2ac7c03f6aaf7c5e5968bbebcfa6e0d85eb1ffd7
alisam1 <alisa-m2@yandex.ru>
2019-01-20 22:50:21 +0300

    Updates

---
+++ precache-manifest.54653ef501fa05bbcef0b915f1d8e453.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "06a04537db9294681f93bc12bd1d9c53",
+    "url": "/dataduck_test/static/media/open_sans.06a04537.eot"
+  },
+  {
+    "revision": "50d1ef418d0fbb50dc05",
+    "url": "/dataduck_test/static/css/main.82ebd408.chunk.css"
+  },
+  {
+    "revision": "701e80665e0aa5267d43",
+    "url": "/dataduck_test/static/js/1.701e8066.chunk.js"
+  },
+  {
+    "revision": "03ceb9316984e6044229",
+    "url": "/dataduck_test/static/js/runtime~main.03ceb931.js"
+  },
+  {
+    "revision": "018985645aef95ca5b31829e2d2e1923",
+    "url": "/dataduck_test/static/media/user.01898564.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/dataduck_test/static/media/open_sans.d41d8cd9.woff"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/dataduck_test/static/media/open_sans_bold.d41d8cd9.woff"
+  },
+  {
+    "revision": "c56a7e34eff771e4196c4f54c4c9016b",
+    "url": "/dataduck_test/static/media/open_sans.c56a7e34.woff2"
+  },
+  {
+    "revision": "ef5ed4e5ea320d7398019604ba698ecb",
+    "url": "/dataduck_test/static/media/open_sans_bold.ef5ed4e5.woff2"
+  },
+  {
+    "revision": "584b9fdf9669bcb8230078585b40b059",
+    "url": "/dataduck_test/static/media/SFNS-Regular.584b9fdf.woff"
+  },
+  {
+    "revision": "dba2a2f394434bc455cbd5cb00afe87d",
+    "url": "/dataduck_test/static/media/SFNS-Bold.dba2a2f3.woff"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/dataduck_test/static/media/open_sans.629a55a7.ttf"
+  },
+  {
+    "revision": "50d1ef418d0fbb50dc05",
+    "url": "/dataduck_test/static/js/main.50d1ef41.chunk.js"
+  },
+  {
+    "revision": "50145685042b4df07a1fd19957275b81",
+    "url": "/dataduck_test/static/media/open_sans_bold.50145685.ttf"
+  },
+  {
+    "revision": "33696c1691b434f58dd2887f2fa2a30f",
+    "url": "/dataduck_test/static/media/open_sans_bold.33696c16.eot"
+  },
+  {
+    "revision": "712d205b35391e52312373ad08ce112d",
+    "url": "/dataduck_test/static/media/open_sans_bold.712d205b.svg"
+  },
+  {
+    "revision": "016f64a730f315ef92d72658d4b2204f",
+    "url": "/dataduck_test/static/media/open_sans.016f64a7.svg"
+  },
+  {
+    "revision": "9e14b4e72dec1db9a60d2636bbfe64f2",
+    "url": "/dataduck_test/static/media/SFNS-Regular.9e14b4e7.ttf"
+  },
+  {
+    "revision": "a9e4f259deea0220b6c83a6bf626c06f",
+    "url": "/dataduck_test/static/media/SFNS-Bold.a9e4f259.ttf"
+  },
+  {
+    "revision": "e82e20e1f2704391b4a333d68010f0ee",
+    "url": "/dataduck_test/static/media/rectangle.e82e20e1.png"
+  },
+  {
+    "revision": "1e0e3713fc74913168ae6ac613784ddc",
+    "url": "/dataduck_test/static/media/laptop.1e0e3713.png"
+  },
+  {
+    "revision": "23b1b908ef17d17d653d208d3b97423a",
+    "url": "/dataduck_test/static/media/ipad.23b1b908.png"
+  },
+  {
+    "revision": "661a326178408b58c8bad4bd67c67686",
+    "url": "/dataduck_test/static/media/mobile.661a3261.png"
+  },
+  {
+    "revision": "fe5c568ff2d3f1cf61e84ad8a2ae6aca",
+    "url": "/dataduck_test/index.html"
+  }
+];
